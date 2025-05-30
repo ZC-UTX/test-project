4abe87711a9d9c74912c2e54903cef60bed755d4
张辰 <3520284090@qq.com>
2025-05-30 09:59:17 +0800

    works:go.mod修改

--- works/works.pb.go
+++ works/works.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.19.4
 // source: works.proto
 
-package __
+package works
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
--- works/works_grpc.pb.go
+++ works/works_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v3.19.4
 // source: works.proto
 
-package __
+package works
 
 import (
 	context "context"
